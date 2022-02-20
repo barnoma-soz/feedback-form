c585a91ef814537d3e3e632f18ed15f9cf057603
Akhmedov Farkhod <a.farkhod@gmail.com>
2022-02-20 22:52:28 +0500

    Добавлена разметка для формы обратной связи

---
+++ resources/js/Layouts/Default.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex flex-col items-center justify-center min-h-screen p-4">
+    <slot></slot>
+  </div>
+</template>
--- resources/js/Pages/FeedbackForm.vue
+++ resources/js/Pages/FeedbackForm.vue
@@ -1,3 +1,26 @@
 <template>
-  Feedback form
+  <h1 class="text-2xl font-semibold text-blue-700">Обратная связь</h1>
+  <form class="border border-gray-300 rounded-lg p-6 mt-6 space-y-3">
+    <div>
+      <label for="name">Имя</label>
+      <input type="text" id="name" placeholder="Введите имя">
+    </div>
+    <div>
+      <label for="phone">Телефон</label>
+      <input type="text" id="phone" placeholder="Введите телефон">
+    </div>
+    <div>
+      <label for="message">Сообщение</label>
+      <textarea id="message" cols="30" rows="10" placeholder="Введите сообщение"></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Отправить</button>
+  </form>
 </template>
+
+<script>
+import Layout from "../Layouts/Default.vue";
+
+export default {
+  layout: Layout,
+}
+</script>
